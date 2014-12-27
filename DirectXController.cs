b78d005bc31bee4c065b0eec6a7d0d61fb1eb4d9
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-12-27 15:59:53 +0200

    version 1.4.5

--- DirectXController.cs
+++ DirectXController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using LibNoise.Unity.Operator;
-using UnityEngine;
 using SharpDX.DirectInput;
 
 namespace KSPAdvancedFlyByWire
