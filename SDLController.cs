6e2da799d82b95d073ca40ee69beea6181012cf4
Andreas Grois <andi@grois.info>
2018-03-25 18:11:44 +0200

    Add condition to only push back events on Linux.
    I think Unity on Windows does not use SDL, but I don't have a Windows box to confirm.

--- SDLController.cs
+++ SDLController.cs
@@ -85,7 +85,9 @@ namespace KSPAdvancedFlyByWire
             SDL.SDL_JoystickUpdate();
             SDL.SDL_Event ev;
             SDL.SDL_PollEvent(out ev);
+#if LINUX
             SDL.SDL_PushEvent(ref ev);
+#endif
         }
 
         public override string GetControllerName()
