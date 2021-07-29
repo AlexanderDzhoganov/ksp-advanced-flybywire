531d199ead6f4f7d8b43fc61b971eb746bba5988
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-07-28 20:40:26 -0400

    Renamed DLL for CKAN compatibility  Added AssemblyFileVersion  Updated version file for 1.12

--- deploy.bat
+++ deploy.bat
@@ -13,8 +13,14 @@ set GAMEDATA="GameData"
 set SUPPORTFILES=SUPPORTFILES
 set VERSIONFILE=%GAMEDIR%.version
 
+set DP0=r:\dp0\kspdev
+
 copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%\Plugins"
+copy /Y "%1%3".pdb "%GAMEDATA%\%GAMEDIR%\Plugins"
+
 copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
 
 xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
-xcopy /y /s /i %SUPPORTFILES% %H%
+xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%DP0%\GameData\%GAMEDIR%"
+
+pause
